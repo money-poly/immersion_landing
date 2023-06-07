26c14efc9939b3815e5e64f23ff3e4c1ce8ae04f
mouse0429 <mouse0429@naver.com>
2023-06-07 18:09:39 +0900

    프로젝트 초기화

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      pretendard: ["Pretendard", "sans-serif"],
+    }
+  },
+  plugins: [],
+}
+
